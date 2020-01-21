dfb4c9cee34315f25be717cfa7b37a11ebf694b1
Sleeyax <yourd3veloper@gmail.com>
2020-01-21 20:18:49 +0100

    Create base controller

---
+++ PcFruit/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PcFruit.Models;
+
+namespace PcFruit.Controllers
+{
+    public abstract class BaseController
+    {
+        protected readonly PcfruitContext _context;
+        
+        protected BaseController(PcfruitContext context)
+        {
+            _context = context;
+        }
+    }
+}
