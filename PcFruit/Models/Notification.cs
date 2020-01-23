9ba8e16a9bd24b346d91d4d1f56184fb8d0f5c33
unknown <rubenhoremans1999@gmail.com>
2020-01-23 14:57:16 +0100

    sms

--- PcFruit/Models/Notification.cs
+++ PcFruit/Models/Notification.cs
@@ -9,14 +9,14 @@ namespace PcFruit.Models
     {
         public int NotificationID { get; set; }
         public int ModuleID { get; set; }
-        public string PhoneNumber { get; set; }
+        public int UserID { get; set; }
         public int MinTemp { get; set; }
         public int MinRH { get; set; }
         public int MaxTemp { get; set;}
         public int MaxRH { get; set; }
 
         public Module module { get; set; }
-
+        public User user { get; set; }
         public bool IsActive { get; set; }
     }
 }
--- PcFruit/Models/User.cs
+++ PcFruit/Models/User.cs
@@ -13,10 +13,14 @@ namespace PcFruit.Models
         public string Voornaam { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string PhoneNumber { get; set; }
         public string Salt { get; set; }
+        public bool hasAccess { get; set; }
 
         [NotMapped]
         public string Token { get; set; }
+
+        public ICollection<Notification> Notifications { get; set; }
 
     }
 }
